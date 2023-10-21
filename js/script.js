11bc6e69149a264db68e079cc02095a211c23344
Aditya Patel <142483507+AdityaPtl07@users.noreply.github.com>
2023-10-21 12:22:00 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,54 +1,60 @@
-const textbox = document.getElementById('txtbox');
-let oldvals = [];
-let c = 0;
-
-function getInput(){
-    const txt = textbox.value;
-    return txt;
-}
-
-function setOutput(output){
-    if (c==0)
-        oldvals.push(textbox.value);
-    oldvals.push(output)
-    textbox.innerText = output;
-    textbox.value = output;
-    c+=1;
-}
-
 //Buttons
 function undo(){
-    if (c!=0){
-        if (textbox.value === oldvals[c]){
-            textbox.innerText = oldvals[c-1];
-            textbox.value = oldvals[c-1];
-            c-=1;
-        }
-        else{
-            textbox.innerText = oldvals[c];
-            textbox.value = oldvals[c];
-        }
-    }
+    document.execCommand('undo', false, null);
 }
 
 function redo(){
-    if (c<oldvals.length-1){
-        textbox.innerText = oldvals[c+1];
-        textbox.value = oldvals[c+1];
-        c+=1;
-    }
+    document.execCommand('redo', false, null);
 }
 
-function justifyCenter() {
-    var editor = document.getElementById("mainEditor");
-    editor.style.textAlign = "center";
+function orderedList(){
+    document.execCommand('insertOrderedList', false, null);
 }
 
-function toUpper(){
-    setOutput(getInput().toUpperCase());
+function unorderedList(){
+    document.execCommand('insertUnorderedList', false, null);
 }
 
-function toLower(){
-    setOutput(getInput().toLowerCase());
+function bold() {
+    document.execCommand('bold', false, null);
+}
+
+function italic(){
+    document.execCommand('italic', false, null);
+}
+
+function underline(){
+    document.execCommand('underline', false, null);
+}
+
+function strike(){
+    document.execCommand('strikeThrough', false, null);
+}
+
+function justifyLeft(){
+    document.execCommand('justifyLeft', false, null);
+}
+
+function justifyCenter(){
+    document.execCommand('justifyCenter', false, null);
+}
+
+function justifyRight(){
+    document.execCommand('justifyRight', false, null);
+}
+
+function chooseColor() {
+    var mycolor = document.getElementById("myColor").value;
+    document.execCommand('foreColor', false, mycolor);
+}
+
+function changeFont() {
+    var myFont = document.getElementById("input-font").value;
+    document.execCommand('fontName', false, myFont);
+}
+
+function changeSize() {
+    var mysize = document.getElementById("fontSize").value;
+    document.execCommand('fontSize', false, mysize);
 }
 
